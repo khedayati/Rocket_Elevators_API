d4466794f6f8e1d4611980838bd3f9d171630706
YenXm <moonvoyager2@protonmail.com>
2021-10-29 16:20:06 -0400

    added authorization method custom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery prepend: true, with: :exception
+    
+
+    def authenticate_employee_user!
+        authenticate_user! 
+        unless current_user.employees.exists?
+            flash[:flash] = "Unauthorized Access!"
+            redirect_to destroy_user_session_path 
+
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get '/quote', to: 'home#quote'
   get '/index', to: 'home#index'
   post '/create_quotes' =>'quotes#create'
+  get '/session/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
