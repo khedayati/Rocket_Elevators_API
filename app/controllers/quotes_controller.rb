435c8050e2c69dfc24e7d6130b8d4bbd0cf4eb3d
YenXm <moonvoyager2@protonmail.com>
2021-10-29 14:12:49 -0400

    fixed quote

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -65,7 +65,9 @@ class QuotesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def quote_params
-      params.permit(:building_type, :amount_elevators, :product_line, :installation_fees, :total_cost)
+      params.permit(:building_type, :amount_elevators, :product_line, :installation_fees, :total_cost,
+        :amount_floors, :amount_basements, :amount_parking_spots, :maximum_occupancy, :amount_apartments,
+        :amount_companies, :amount_corporations, :business_hours)
       
     end
 end
