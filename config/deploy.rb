9ac689aaf9fdb69cd918bc937ac7435603044d04
YenXm <moonvoyager2@protonmail.com>
2021-10-29 10:35:33 -0400

    Added Deployement file

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@ lock "~> 3.11.0"
 
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
-set :application, "YOUR_FULL_NAME"
-set :repo_url, "https://github.com/MY-ACCOUNT/MY-REPO.git"
+set :application, "***REMOVED***"
+set :repo_url, "***REMOVED***"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
