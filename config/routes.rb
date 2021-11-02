40f88ddec10fb43c32b4f1fbc95726bda1cb5091
Phanourith <phanourithkeosavang@gmail.com>
2021-11-02 13:47:41 -0400

    almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
@@ -6,9 +8,9 @@ Rails.application.routes.draw do
   get '/commercial', to: 'home#commercial'
   get '/quote', to: 'home#quote'
   get '/index', to: 'home#index'
-  post '/create_quotes' =>'quotes#create'
-  get '/users/:id' , to: 'users#show'
-  get '/users/:id/edit', to: 'users#edit', :as => :user
+  post '/create_quotes' => 'quotes#create'
+  get '/users/:id', to: 'users#show'
+  get '/users/:id/edit', to: 'users#edit', as: :user
   patch '/users/:id/edit' => 'users#update'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/system/employees_test.rb
+++ test/system/employees_test.rb
@@ -1,47 +1,49 @@
-require "application_system_test_case"
+# frozen_string_literal: true
+
+require 'application_system_test_case'
 
 class EmployeesTest < ApplicationSystemTestCase
   setup do
     @employee = employees(:one)
   end
 
-  test "visiting the index" do
+  test 'visiting the index' do
     visit employees_url
-    assert_selector "h1", text: "Employees"
+    assert_selector 'h1', text: 'Employees'
   end
 
-  test "creating a Employee" do
+  test 'creating a Employee' do
     visit employees_url
-    click_on "New Employee"
+    click_on 'New Employee'
 
-    fill_in "First name", with: @employee.first_name
-    fill_in "Function", with: @employee.function
-    fill_in "Last name", with: @employee.last_name
-    click_on "Create Employee"
+    fill_in 'First name', with: @employee.first_name
+    fill_in 'Function', with: @employee.function
+    fill_in 'Last name', with: @employee.last_name
+    click_on 'Create Employee'
 
-    assert_text "Employee was successfully created"
-    click_on "Back"
+    assert_text 'Employee was successfully created'
+    click_on 'Back'
   end
 
-  test "updating a Employee" do
+  test 'updating a Employee' do
     visit employees_url
-    click_on "Edit", match: :first
+    click_on 'Edit', match: :first
 
-    fill_in "First name", with: @employee.first_name
-    fill_in "Function", with: @employee.function
-    fill_in "Last name", with: @employee.last_name
-    click_on "Update Employee"
+    fill_in 'First name', with: @employee.first_name
+    fill_in 'Function', with: @employee.function
+    fill_in 'Last name', with: @employee.last_name
+    click_on 'Update Employee'
 
-    assert_text "Employee was successfully updated"
-    click_on "Back"
+    assert_text 'Employee was successfully updated'
+    click_on 'Back'
   end
 
-  test "destroying a Employee" do
+  test 'destroying a Employee' do
     visit employees_url
     page.accept_confirm do
-      click_on "Destroy", match: :first
+      click_on 'Destroy', match: :first
     end
 
-    assert_text "Employee was successfully destroyed"
+    assert_text 'Employee was successfully destroyed'
   end
 end
