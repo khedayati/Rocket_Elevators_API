a529821ecf8e59c08e228ecc25a00464303d32a5
YenXm <moonvoyager2@protonmail.com>
2021-11-03 19:55:04 -0400

    Added admin new panel and fixed batterie syntax error in Column

---
+++ app/admin/elevators.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Elevator do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :column_id, :serial_number, :model, :type, :status, :Date_of_commissioning, :date_of_last_inspection, :certificate_of_inspection, :information, :notes
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:column_id, :serial_number, :model, :type, :status, :Date_of_commissioning, :date_of_last_inspection, :certificate_of_inspection, :information, :notes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
