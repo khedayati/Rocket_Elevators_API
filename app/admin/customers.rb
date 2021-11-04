0e05f3f35ec486e97dd5de6bb16e9b495fc6b387
YenXm <moonvoyager2@protonmail.com>
2021-11-04 08:10:06 -0400

    Fixed customer active_admin permit_params and controller require params

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -6,7 +6,8 @@ ActiveAdmin.register Customer do
   # Uncomment all parameters which should be permitted for assignment
   #
   # permit_params :user_id, :customer_creation_date, :company_name, :company_headquarters_address, :full_name_of_the_company, :contact, :company_contact_phone, :email_of_the, :company_contact, :company_description, :full_name_of, :service_technical_authority, :technical_authority_phone_for, :service, :technical_manager_email_for_service
-  #
+  
+    permit_params :user_id, :customer_creation_date, :company_name, :company_headquarters_address, :full_name_of_the_company_contact, :company_contact_phone, :email_of_the_company_contact, :company_description, :full_name_of, :service_technical_authority, :technical_authority_phone_for_service, :technical_manager_email_for_service
   # or
   #
   # permit_params do
