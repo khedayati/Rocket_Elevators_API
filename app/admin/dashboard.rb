4f454e8fb27457301dcaec3aeada7ed0913ba796
YenXm <moonvoyager2@protonmail.com>
2021-11-04 17:12:11 -0400

    chartkick shit

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,34 +1,21 @@
 # frozen_string_literal: true
-
+#= require chartkick
 
 ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    div class: 'blank_slate_container', id: 'dashboard_default_message' do
-      span class: 'blank_slate' do
-        span I18n.t('active_admin.dashboard_welcome.welcome')
-        small I18n.t('active_admin.dashboard_welcome.call_to_action')
-      end
-    end
 
-  columns do
-    column max_width: "200px", min_width: "100px" do
-      span "Column # 1"
-    end
-    column do
-      span "Column # 2"
-    end
-  end
-  table_for current_user do
-    column(:email) { |payment| payment } 
-    column "First Name",     :first_name, max_width: "50px"
-    column "Last Name",      :last_name
-    column "Create At",      :created_at
-  end
+    
+    connection = PG::Connection.new(host:'localhost',port:'5432',dbname:'data_warehouse',user:'yen',password:'password')
+    timeline [
+      ["Washington", "1789-04-29", "1797-03-03"],
+      ["Adams", "1797-03-03", "1801-03-03"],
+      ["Jefferson", "1801-03-03", "1809-03-03"]
+    ]
+    line_chart [{name: "Workout", data: {"2021-01-01" => 3, "2021-01-02" => 4}},{name: "Call parents", data: {"2021-01-01" => 5, "2021-01-02" => 3}}]
 
-
-
+    
     # Here is an example of a simple dashboard with columns and panels.
     #
     # "hello"
