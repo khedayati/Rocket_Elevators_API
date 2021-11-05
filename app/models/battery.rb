643a0ed8a2618d9ffcc0f9e6631768f1aa70af00
YenXm <moonvoyager2@protonmail.com>
2021-11-05 16:37:35 -0400

    Fixed back office delete and object string show

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -1,5 +1,10 @@
 class Battery < ApplicationRecord
   belongs_to :building
   belongs_to :employee
-  has_many :columns
+  has_many :columns, dependent: :destroy
+
+
+  def to_s
+    "Battery #" + self.id.to_s
+  end
 end
--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -1,3 +1,7 @@
 class Elevator < ApplicationRecord
   belongs_to :column
+
+  def to_s
+    "Elevator #" + self.id.to_s
+  end
 end
