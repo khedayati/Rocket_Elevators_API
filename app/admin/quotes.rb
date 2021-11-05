c82a0d2618d9dc4a1ae49dbc29d87a163258f8f6
YenXm <moonvoyager2@protonmail.com>
2021-11-05 13:52:17 -0400

    Fixed Admin Panel permit param

--- app/admin/quotes.rb
+++ app/admin/quotes.rb
@@ -1,18 +1,18 @@
-# frozen_string_literal: true
-
 ActiveAdmin.register Quote do
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :building_type, :amount_elevators, :product_line, :installation_fees, :total_cost
+  # permit_params :building_type, :amount_elevators, :product_line, :installation_fees, :total_cost, :amount_floors, :amount_basements, :amount_parking_spots, :maximum_occupancy, :amount_apartments, :amount_companies, :amount_corporations, :business_hours, :email, :company_name
   #
   # or
-  #
-  # permit_params do
-  #   permitted = [:building_type, :amount_elevators, :product_line, :installation_fees, :total_cost]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  
+  permit_params do
+    permitted = [:building_type, :amount_elevators, :product_line, :installation_fees, :total_cost, :amount_floors, :amount_basements, :amount_parking_spots, :maximum_occupancy, :amount_apartments, :amount_companies, :amount_corporations, :business_hours, :email, :company_name]
+    permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+  
 end
