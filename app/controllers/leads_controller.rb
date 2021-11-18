1aa6deaff084baf476918e1bc2852056423847df
johnnybigoo <jotalico@icloud.com>
2021-11-18 17:26:08 -0500

    Integration-Sendgrid-API

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -36,22 +36,6 @@ class LeadsController < ApplicationController
         format.json { render json: @lead.errors, status: :unprocessable_entity }
       end
     end
-
-    # puts 'sendgrid'
-
-    # mail = Mail.new
-    # mail.from = Email.new(email: 'rocketelevators.team@gmail.com')
-    # custom = Personalization.new
-    # custom.add_to(Email.new(email: @lead.email))
-    # custom.add_dynamic_template_data({
-    #     "fullName" => @lead.full_name,
-    #     "projectName" => @lead.project_name
-    # })
-    # mail.add_personalization(custom)
-    # mail.template_id = 'd-a139386f69394a49ab3cf9c478b80768'
-
-    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    # response = sg.client.mail._('send').post(request_body: mail.to_json)
   end
 
 
--- app/services/sendgrid.rb
+++ app/services/sendgrid.rb
@@ -8,19 +8,18 @@ class Sendgrid
     @lead = lead
   end
 
-  def call 
-    # puts 'sendgrid'
+  def call
     mail = Mail.new
-    mail.from = Email.new(email: 'rocketelevators.team@gmail.com')
+    mail.from = Email.new(email: ENV['EMAIL_FROM_CONTACT'])
     custom = Personalization.new
-    custom.add_to(Email.new(email: @lead.email))
+    custom.add_to(Email.new(email: lead.email))
     custom.add_dynamic_template_data({
-      "fullName" => @lead.full_name,
-      "projectName" => @lead.project_name
+      "fullName" => lead.full_name,
+      "projectName" => lead.project_name
     })
     
     mail.add_personalization(custom)
-    mail.template_id = 'd-a139386f69394a49ab3cf9c478b80768'
+    mail.template_id = ENV['TEMPLATE_ID']
 
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
