5a018ec430b11e0ee724c8d60438b128216cdef8
Head0 <tsken010@outlook.com>
2021-11-18 11:37:12 -0500

    Dropbox-working

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,12 +1,16 @@
 class Lead < ApplicationRecord
     has_one_attached :contact_attachment_file
+    after_create :upload_dropbox
     def attachment_path
-        
+
     end
 
     def to_s
         "Lead #" + self.id.to_s
     end
-    
 
+    private
+    def upload_dropbox
+        Dropbox.new(self).call
+    end
 end
