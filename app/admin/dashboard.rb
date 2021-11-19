318a9ca9781fa590f11d89ca19c63dd4304e882e
YenXm <moonvoyager2@protonmail.com>
2021-11-19 18:31:16 -0500

    ibm 2

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,7 +12,7 @@ authenticator = Authenticators::IamAuthenticator.new(
 text_to_speech = TextToSpeechV1.new(
  authenticator: authenticator,
 )
-text_to_speech.service_url = "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/4a765487-f171-4f1d-9e6b-1b5f3cccd0b1/v1/synthezise"
+text_to_speech.service_url = "https://api.us-east.text-to-speech.watson.cloud.ibm.com/instances/4a765487-f171-4f1d-9e6b-1b5f3cccd0b1"
 text_to_speech.configure_http_client(disable_ssl_verification: true)
 
 
