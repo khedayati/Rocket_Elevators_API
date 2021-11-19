9faac8f4770e426bead8bb02f9b6b9b88414b152
Head0 <tsken010@outlook.com>
2021-11-19 10:49:04 -0500

    update deploy.rb production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,22 +1,21 @@
-# frozen_string_literal: true
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '99.79.123.96', user: 'ubuntu', roles: %w[app db web]
+server "99.79.123.96", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 app = ENV['APP']
-app = 'LEVY' if app.nil? || app.empty?
+if app.nil? or app.empty?
+  app = "KhashayarHedayati" 
+end
 set :application, app
-set :rails_env, 'development'
-set :bundle_without, 'production'
+set :rails_env, "development"
+set :bundle_without, "production"
 set :deploy_to, "/home/ubuntu/apps/#{app}"
-set :linked_dirs, %w[tmp/pids tmp/sockets log]
-set :linked_files, %w[config/database.yml]
-#set :linked_files, %w[config/local_env.yml]
+set :linked_dirs, %w{tmp/pids tmp/sockets log}
+set :linked_files, %w{config/database.yml}
 
 # role-based syntax
 # ==================
@@ -26,9 +25,11 @@ set :linked_files, %w[config/database.yml]
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w[ubuntu@99.79.123.96]
-role :web, %w[ubuntu@99.79.123.96]
-role :db,  %w[ubuntu@99.79.123.96]
+role :app, %w{ubuntu@99.79.123.96}
+role :web, %w{ubuntu@99.79.123.96}
+role :db,  %w{ubuntu@99.79.123.96}
+
+
 
 # Configuration
 # =============
@@ -37,6 +38,8 @@ role :db,  %w[ubuntu@99.79.123.96]
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
+
+
 
 # Custom SSH Options
 # ==================
