91888ceb2ea926aac8a510091606d4dc34f74777
YenXm <moonvoyager2@protonmail.com>
2021-11-19 15:47:16 -0500

    dropbox fix

--- app/services/dropbox.rb
+++ app/services/dropbox.rb
@@ -8,14 +8,16 @@ class Dropbox
   end
 
   def call 
-    client = DropboxApi::Client.new(ENV['DROPBOX_ACCESS_TOKEN'])
-    # client.create_folder("/Test")
-    filename_path = ActiveStorage::Blob.service.send(:path_for, @lead.contact_attachment_file.key)
-    new_filename = "#{@lead.company_name.parameterize}/#{@lead.contact_attachment_file.filename.to_s}"
-    File.open(filename_path) do |f|
-      client.upload_by_chunks "/ContactUs/#{new_filename}", f
-    end
+    if @lead.contact_attachment_file.attached?
+      client = DropboxApi::Client.new(ENV['DROPBOX_ACCESS_TOKEN'])
+      # client.create_folder("/Test")
+      filename_path = ActiveStorage::Blob.service.send(:path_for, @lead.contact_attachment_file.key)
+      new_filename = "#{@lead.company_name.parameterize}/#{@lead.contact_attachment_file.filename.to_s}"
+      File.open(filename_path) do |f|
+        client.upload_by_chunks "/ContactUs/#{new_filename}", f
+      end
 
-    @lead.contact_attachment_file.purge
+      @lead.contact_attachment_file.purge
+    end
   end
 end
